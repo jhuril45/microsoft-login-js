7b344c1d4dac1b52ca18463959e5e94ea77fbace
jhuril45 <jhuril45@gmail.com>
2023-01-06 10:07:18 +0800

    Updates on index

--- src/index.js
+++ src/index.js
@@ -1,11 +1,4 @@
 import directive from "./directive";
-import { MicrosoftAuth } from './MicrosoftAuth'
-
-const Auth = (value) => new MicrosoftAuth(value);
-
-export {
-  Auth
-}
 
 const plugin = {
   install(Vue) {
